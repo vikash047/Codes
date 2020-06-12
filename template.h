4aee76e01763010206ea3653fed9112e9b2e268f
vikash047 <vikash.ku0047@gmail.com>
2020-06-12 12:47:01 +0530

    Added new Content

--- template.h
+++ template.h
@@ -64,6 +64,19 @@ vector<string> split(string s, string del) {
         return ans;
 }
 
+
+struct pair_hash
+{
+	template <class T1, class T2>
+	std::size_t operator () (std::pair<T1, T2> const &pair) const
+	{
+		std::size_t h1 = std::hash<T1>()(pair.first);
+		std::size_t h2 = std::hash<T2>()(pair.second);
+
+		return h1 ^ h2;
+	}
+};
+
 int main(int argc, char const *argv[])
 {
     /* code */
